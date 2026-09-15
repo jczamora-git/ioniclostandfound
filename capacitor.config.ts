2eb26ee3fea23ebeae652b61faed1bdce74f9795
jczamora-git <johnchristopherkingzamora@gmail.com>
2026-09-15 09:22:36 +0800

    Lost and Found

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.ioniclostandfound',
-  appName: 'IonicLostAndFound',
+  appName: 'Lost and Found',
   webDir: 'dist'
 };
 
